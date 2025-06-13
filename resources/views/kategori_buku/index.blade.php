f11b0b6d28693371697d09fda4eb7256d8d4b0f3
alyaalawiyah <alyaaalawiyah00@gmail.com>
2025-06-13 16:03:49 +0700

    Tambah file baru

--- resources/views/kategori_buku/index.blade.php
+++ resources/views/kategori_buku/index.blade.php
@@ -1,1 +1,18 @@
-<h1>Daftar Kategori Buku</h1>
+@extends('layout.app')
+
+@section('title', 'Kategori Buku')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Kategori Buku</h5>
+
+                    <div class="text-end mb-3">
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
