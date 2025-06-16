f07db35d2fbc67a57292dc044b6be334d795beff
alyaalawiyah <alyaaalawiyah00@gmail.com>
2025-06-16 15:18:22 +0700

    ubah halaman KategoriBukuSeeder

--- database/seeders/KategoriBukuSeeder.php
+++ database/seeders/KategoriBukuSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class KategoriBukuSeeder extends Seeder
 {
@@ -13,9 +14,13 @@ class KategoriBukuSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('kategori_buku')->insert([
+       DB::table('kategori_buku')->truncate();
+
+       Schema::enableForeignKeyConstraints();
+
+       DB::table('kategori_buku')->insert([
             ['nama_kategori' => 'Fiksi'],
             ['nama_kategori' => 'Non Fiksi'],
-        ]);
+       ]);
     }
 }
