c3234f3c75e93f12378e12683f7d36bd72c3d774
alyaalawiyah <alyaaalawiyah00@gmail.com>
2025-06-18 11:48:49 +0700

    tambah migrasi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
@@ -52,6 +53,7 @@ class AuthController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Session::regenerate();
         return redirect()->route('login')->with('success', 'Registrasi berhasil. Silakan login.');
     }
 }
