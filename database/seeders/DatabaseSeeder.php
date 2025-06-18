a134b3210283607c93bb2f52b5af55e82138fe91
alyaalawiyah <alyaaalawiyah00@gmail.com>
2025-06-18 15:04:29 +0700

    ubah DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,9 +9,7 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
         $this->call([
